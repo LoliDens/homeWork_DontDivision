6d3b3fa2ed163217b8eb223f9f14b3979111ae82
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-18 00:53:12 +0500

    Добавьте файлы проекта.

---
+++ homeWork_DontDivision/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_DontDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int lowerLimitRandom = 1;
+            int upperLimitRandom = 28;
+            int number = random.Next(lowerLimitRandom, upperLimitRandom);
+            int upperLimitCycle = 999;
+            int lowerLimitCycle = 100;
+            int multiples;
+
+            Console.WriteLine($"Числа кратнык {number} от {lowerLimitCycle} до {upperLimitCycle}: ");
+
+            for (int i = lowerLimitCycle; i <= upperLimitCycle; i++) 
+            {
+                multiples = i;
+
+                while (multiples > 0) 
+                {
+                    multiples -= number;
+                }
+
+                if (multiples == 0) 
+                {
+                    Console.Write($"{i} ");
+                }            
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
