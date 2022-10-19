3fb482bddeafc5fde1cf17da504d89d43febcd5b
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-19 18:04:04 +0500

    добавлена новая переменная программа теперь выводит количесто кратных чисел а, не их самих

--- homeWork_DontDivision/Program.cs
+++ homeWork_DontDivision/Program.cs
@@ -16,9 +16,8 @@ namespace homeWork_DontDivision
             int number = random.Next(lowerLimitRandom, upperLimitRandom);
             int upperLimitCycle = 999;
             int lowerLimitCycle = 100;
+            int amountMultiples = 0;
             int multiples;
-
-            Console.WriteLine($"Числа кратнык {number} от {lowerLimitCycle} до {upperLimitCycle}: ");
 
             for (int i = lowerLimitCycle; i <= upperLimitCycle; i++) 
             {
@@ -31,10 +30,11 @@ namespace homeWork_DontDivision
 
                 if (multiples == 0) 
                 {
-                    Console.Write($"{i} ");
+                    amountMultiples += 1;
                 }            
             }
 
+            Console.WriteLine($"Количество трехзначных натуральных чисел, которые кратны {number}: {amountMultiples}");
             Console.ReadKey();
         }
     }
